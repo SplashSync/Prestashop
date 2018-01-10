da4c98c9f8e045871296027d109b91155cda3a99
BadPixxel <eshop.bpaquier@gmail.com>
2018-01-10 20:38:35 +0100

    Work On Travis Ci

--- build/install_module.sh
+++ build/install_module.sh
@@ -1,8 +1,14 @@
 
 echo Install SplashSync Module
 
+# Build Module Dependencies
+cd $MODULE_DIR
+composer update --prefer-dist --no-interaction --no-progress --no-dev
+cd $TRAVIS_BUILD_DIR
+
 # Move Module Contents to Install Folder
 mkdir     $TRAVIS_BUILD_DIR/modules/splashsync
 cp -Rf    $MODULE_DIR/modules/splashsync/*              $TRAVIS_BUILD_DIR/modules/splashsync/      
 cp -f     $MODULE_DIR/build/phpunit.xml.dist            $TRAVIS_BUILD_DIR/phpunit.xml      
 ls -al    $TRAVIS_BUILD_DIR/modules/splashsync
+
