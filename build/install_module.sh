07232b435d3d873a085b2c38f90462785e827486
BadPixxel <eshop.bpaquier@gmail.com>
2018-01-10 19:56:03 +0100

    Work On Travis Ci

--- build/install_module.sh
+++ build/install_module.sh
@@ -3,5 +3,5 @@ echo Install SplashSync Module
 
 # Move Module Contents to Install Folder
 mkdir     $TRAVIS_BUILD_DIR/modules/splashsync
-mv -f     $TMP_BUILD_DIR/modules/splashsync/*             $TRAVIS_BUILD_DIR/modules/splashsync/      
+mv -f     $MODULE_DIR/modules/splashsync/*             $TRAVIS_BUILD_DIR/modules/splashsync/      
 ls -al    $TRAVIS_BUILD_DIR/modules/splashsync
