54fa920acc79ef1ecde6d570c96482b33d4c6ba2
BadPixxel <eshop.bpaquier@gmail.com>
2018-01-10 20:27:08 +0100

    Work On Travis Ci

--- build/install_module.sh
+++ build/install_module.sh
@@ -3,5 +3,6 @@ echo Install SplashSync Module
 
 # Move Module Contents to Install Folder
 mkdir     $TRAVIS_BUILD_DIR/modules/splashsync
-mv -f     $MODULE_DIR/modules/splashsync/*             $TRAVIS_BUILD_DIR/modules/splashsync/      
+cp -Rf    $MODULE_DIR/modules/splashsync/*              $TRAVIS_BUILD_DIR/modules/splashsync/      
+cp -f     $MODULE_DIR/build/phpunit.xml.dist            $TRAVIS_BUILD_DIR/phpunit.xml      
 ls -al    $TRAVIS_BUILD_DIR/modules/splashsync
--- build/install_webserver.sh
+++ build/install_webserver.sh
@@ -1,9 +1,6 @@
 
 echo Install WebServer
 
-# Disable Xdebug ...
-#phpenv config-rm xdebug.ini
-
 # Apache & php-fpm configuration
 bash travis-scripts/setup-php-fpm.sh
 bash travis-scripts/setup-apache.sh
