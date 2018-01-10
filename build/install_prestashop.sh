b21c57213d6f0aac3c08388c34ac9b84a42abae9
BadPixxel <eshop.bpaquier@gmail.com>
2018-01-10 19:38:02 +0100

    Work On Travis Ci

--- build/install_prestashop.sh
+++ build/install_prestashop.sh
@@ -1,14 +1,6 @@
 
 echo Clone Prestashop
-
-# Move Module Contents to Tmp Folder
-mkdir     $TMP_BUILD_DIR
-mv -f     $TRAVIS_BUILD_DIR/*             $TMP_BUILD_DIR      
-rm -Rf    $TRAVIS_BUILD_DIR/.git
-rm -Rf    $TRAVIS_BUILD_DIR/.gitignore
-rm -Rf    $TRAVIS_BUILD_DIR/.travis.yml
-ls -al    $TRAVIS_BUILD_DIR
-    
+   
 # Clone Prestashop into Build Folder
 cd $TRAVIS_BUILD_DIR
 git clone --depth=50 --branch=master https://github.com/Prestashop/Prestashop.git $TRAVIS_BUILD_DIR
--- build/move_module.sh
+++ build/move_module.sh
@@ -8,12 +8,4 @@ rm -Rf    $TRAVIS_BUILD_DIR/.git
 rm -Rf    $TRAVIS_BUILD_DIR/.gitignore
 rm -Rf    $TRAVIS_BUILD_DIR/.travis.yml
 ls -al    $TRAVIS_BUILD_DIR
-    
-# Clone Prestashop into Build Folder
-cd $TRAVIS_BUILD_DIR
-git clone --depth=50 --branch=master https://github.com/Prestashop/Prestashop.git $TRAVIS_BUILD_DIR
-ls -al  $TRAVIS_BUILD_DIR
 
-
-# PrestaShop configuration
-cp tests/parameters.yml.travis app/config/parameters.yml
