25539ae368e415ed41c3a576e1a7057acccc47b8
BadPixxel <eshop.bpaquier@gmail.com>
2018-01-10 22:46:31 +0100

    Work On Travis Ci

--- build/install_module.sh
+++ build/install_module.sh
@@ -13,8 +13,8 @@ cp -f     $MODULE_DIR/build/phpunit.xml.dist            $TRAVIS_BUILD_DIR/phpuni
 ls -al    $TRAVIS_BUILD_DIR/modules/splashsync
 
 # Setup Module's Default Parameters
-mysql -e "INSERT INTO `prestashop`.`ps_configuration`(`name`, `value`, `date_add`, `date_upd`) VALUES ('SPLASH_WS_ID','0123456789',NOW(), NOW());"
-mysql -e "INSERT INTO `prestashop`.`ps_configuration`(`name`, `value`, `date_add`, `date_upd`) VALUES ('SPLASH_WS_KEY','ThisTokenIsNotSoSecretChangeIt',NOW(), NOW());"
-mysql -e "INSERT INTO `prestashop`.`ps_configuration`(`name`, `value`, `date_add`, `date_upd`) VALUES ('SPLASH_LANG_ID','en-US',NOW(), NOW());"
-mysql -e "INSERT INTO `prestashop`.`ps_configuration`(`name`, `value`, `date_add`, `date_upd`) VALUES ('SPLASH_USER_ID','1',NOW(), NOW());"
+mysql -D prestashop -e "INSERT INTO ps_configuration ( name ,  value ,  date_add ,  date_upd ) VALUES ('SPLASH_WS_ID','0123456789',NOW(), NOW());"
+mysql -D prestashop -e "INSERT INTO ps_configuration ( name ,  value ,  date_add ,  date_upd ) VALUES ('SPLASH_WS_KEY','ThisTokenIsNotSoSecretChangeIt',NOW(), NOW());"
+mysql -D prestashop -e "INSERT INTO ps_configuration ( name ,  value ,  date_add ,  date_upd ) VALUES ('SPLASH_LANG_ID','en-US',NOW(), NOW());"
+mysql -D prestashop -e "INSERT INTO ps_configuration ( name ,  value ,  date_add ,  date_upd ) VALUES ('SPLASH_USER_ID','1',NOW(), NOW());"
 
