c6d6553a72bf0ed7d87306eb57002479003b500b
BadPixxel <eshop.bpaquier@gmail.com>
2018-06-01 00:37:42 +0200

    Wip: Add Grump to Module

--- build/move_module.sh
+++ build/move_module.sh
@@ -4,8 +4,8 @@ echo Move Module to TMP Folder
 # Move Module Contents to Tmp Folder
 mkdir     $MODULE_DIR
 mv -f     $TRAVIS_BUILD_DIR/*             $MODULE_DIR      
-#rm -Rf    $TRAVIS_BUILD_DIR/.git
-#rm -Rf    $TRAVIS_BUILD_DIR/.gitignore
-#rm -Rf    $TRAVIS_BUILD_DIR/.travis.yml
+rm -Rf    $TRAVIS_BUILD_DIR/.git
+rm -Rf    $TRAVIS_BUILD_DIR/.gitignore
+rm -Rf    $TRAVIS_BUILD_DIR/.travis.yml
 ls -al    $TRAVIS_BUILD_DIR
 
