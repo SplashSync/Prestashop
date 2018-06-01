7b5a417bd87059f11826b76d0c1eccca11d1c402
BadPixxel <eshop.bpaquier@gmail.com>
2018-06-01 10:01:30 +0200

    Wip: Add Grump to Module

--- build/install_module.sh
+++ build/install_module.sh
@@ -9,12 +9,12 @@ composer update --prefer-dist --no-interaction --no-progress
 cd $TRAVIS_BUILD_DIR
 
 # Move Module Contents to Install Folder
-#mkdir     $TRAVIS_BUILD_DIR/modules/splashsync
-#cp -Rf    $MODULE_DIR/modules/splashsync/*              $TRAVIS_BUILD_DIR/modules/splashsync/      
-cp -Rf    $MODULE_DIR                                   $TRAVIS_BUILD_DIR      
+mkdir     $TRAVIS_BUILD_DIR/modules/splashsync
+cp -Rf    $MODULE_DIR/modules/splashsync/*              $TRAVIS_BUILD_DIR/modules/splashsync/      
 rm -Rf    $TRAVIS_BUILD_DIR/.git/*      
-cp -Rf    $MODULE_DIR/.git                              $TRAVIS_BUILD_DIR/.git      
+cp -Rf    $MODULE_DIR/.git/*                            $TRAVIS_BUILD_DIR/.git/*      
 cp -f     $MODULE_DIR/build/phpunit.xml.dist            $TRAVIS_BUILD_DIR/phpunit.xml           
+cp -f     $MODULE_DIR/grumphp.yml                       $TRAVIS_BUILD_DIR/grumphp.yml           
 ls -al    $TRAVIS_BUILD_DIR/modules/splashsync
 ls -al    $MODULE_DIR/.git/
 ls -al    $TRAVIS_BUILD_DIR/.git/
