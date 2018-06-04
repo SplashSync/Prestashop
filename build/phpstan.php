fdb72fc4e6b4a071910161e326ac1f4474e915ea
BadPixxel <eshop.bpaquier@gmail.com>
2018-06-04 16:33:56 +0200

    Wip: Add Grump to Module

---
+++ build/phpstan.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Bootstrap Prestashop for Pḧpstan
+ */
+
+require_once dirname(__DIR__) . "/modules/splashsync/vendor/autoload.php";
+require_once dirname(__DIR__) . "/vendor/autoload.php";
+
+//====================================================================//
+// Init Splash for Local Includes 
+Splash\Client\Splash::core();
