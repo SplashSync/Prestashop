8e4633e20a1dfb4932592f7290ba3192d3626e0c
BadPixxel <eshop.bpaquier@gmail.com>
2019-02-25 16:03:28 +0100

    WIP: Upgrade to Splash V2 Standards

--- travis/start_prestashop.sh
+++ travis/start_prestashop.sh
@@ -1,7 +1,10 @@
 
-echo Start Prestashop
-
+echo Compile Prestashop
 composer install --prefer-dist --no-interaction --no-progress --no-dev
-bash tests/check_file_syntax.sh
+
+#echo Check Prestashop Files
+# bash tests/check_file_syntax.sh
+
+echo Install Prestashop
 bash travis-scripts/install-prestashop
 
