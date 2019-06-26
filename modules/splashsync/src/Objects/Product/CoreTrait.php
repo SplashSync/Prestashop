8c1ea94b2bea2b95b3ceb73891443903ba8942c2
BadPixxel <eshop.bpaquier@gmail.com>
2019-06-26 18:29:33 +0200

    Added Filters for Packs & Virtual Products

--- modules/splashsync/src/Objects/Product/CoreTrait.php
+++ modules/splashsync/src/Objects/Product/CoreTrait.php
@@ -28,6 +28,92 @@ use Translate;
 trait CoreTrait
 {
     /**
+     * Build Core Fields using FieldFactory
+     */
+    protected function buildCoreFields()
+    {
+        //====================================================================//
+        // Reference
+        $this->fieldsFactory()->create(SPL_T_VARCHAR)
+            ->Identifier("ref")
+            ->Name(Translate::getAdminTranslation("Reference", "AdminProducts"))
+            ->Description(Translate::getAdminTranslation(
+                'Your internal reference code for this product.',
+                "AdminProducts"
+            ))
+            ->isListed()
+            ->MicroData("http://schema.org/Product", "model")
+            ->isRequired();
+
+        //====================================================================//
+        // Type
+        $this->fieldsFactory()->create(SPL_T_VARCHAR)
+            ->identifier("type")
+            ->name("Type")
+            ->description('Internal product Type')
+            ->group(Translate::getAdminTranslation("Meta", "AdminThemes"))
+            ->microData("http://schema.org/Product", "type")
+            ->isReadOnly();
+    }
+
+    /**
+     * Read requested Field
+     *
+     * @param string $key       Input List Key
+     * @param string $fieldName Field Identifier / Name
+     */
+    protected function getCoreFields($key, $fieldName)
+    {
+        //====================================================================//
+        // READ Fields
+        switch ($fieldName) {
+            //====================================================================//
+            // MAIN INFORMATIONS
+            //====================================================================//
+            case 'ref':
+                $this->out[$fieldName] = $this->getProductReference();
+
+                break;
+            case 'type':
+                $this->out[$fieldName] = $this->getProductType();
+
+                break;
+            default:
+                return;
+        }
+
+        unset($this->in[$key]);
+    }
+
+    /**
+     * Write Given Fields
+     *
+     * @param string $fieldName Field Identifier / Name
+     * @param mixed  $fieldData Field Data
+     */
+    protected function setCoreFields($fieldName, $fieldData)
+    {
+        //====================================================================//
+        // WRITE Field
+        switch ($fieldName) {
+            //====================================================================//
+            // MAIN INFORMATIONS
+            //====================================================================//
+            case 'ref':
+                if ($this->AttributeId) {
+                    $this->setSimple("reference", $fieldData, "Attribute");
+                } else {
+                    $this->setSimple("reference", $fieldData);
+                }
+
+                break;
+            default:
+                return;
+        }
+        unset($this->in[$fieldName]);
+    }
+
+    /**
      * Ready of Product Reference
      *
      * @return string
@@ -77,92 +163,5 @@ trait CoreTrait
         }
 
         return "simple";
-    }
-
-    /**
-     * Build Core Fields using FieldFactory
-     */
-    private function buildCoreFields()
-    {
-        //====================================================================//
-        // Reference
-        $this->fieldsFactory()->create(SPL_T_VARCHAR)
-            ->Identifier("ref")
-            ->Name(Translate::getAdminTranslation("Reference", "AdminProducts"))
-            ->Description(Translate::getAdminTranslation(
-                'Your internal reference code for this product.',
-                "AdminProducts"
-            ))
-            ->isListed()
-            ->MicroData("http://schema.org/Product", "model")
-            ->isRequired();
-
-        //====================================================================//
-        // Type
-        $this->fieldsFactory()->create(SPL_T_VARCHAR)
-            ->identifier("type")
-            ->name("Type")
-            ->description('Internal product Type')
-            ->group(Translate::getAdminTranslation("Meta", "AdminThemes"))
-            ->microData("http://schema.org/Product", "type")
-            ->isListed()
-            ->isReadOnly();
-    }
-
-    /**
-     * Read requested Field
-     *
-     * @param string $key       Input List Key
-     * @param string $fieldName Field Identifier / Name
-     */
-    private function getCoreFields($key, $fieldName)
-    {
-        //====================================================================//
-        // READ Fields
-        switch ($fieldName) {
-            //====================================================================//
-            // MAIN INFORMATIONS
-            //====================================================================//
-            case 'ref':
-                $this->out[$fieldName] = $this->getProductReference();
-
-                break;
-            case 'type':
-                $this->out[$fieldName] = $this->getProductType();
-
-                break;
-            default:
-                return;
-        }
-
-        unset($this->in[$key]);
-    }
-
-    /**
-     * Write Given Fields
-     *
-     * @param string $fieldName Field Identifier / Name
-     * @param mixed  $fieldData Field Data
-     */
-    private function setCoreFields($fieldName, $fieldData)
-    {
-        //====================================================================//
-        // WRITE Field
-        switch ($fieldName) {
-            //====================================================================//
-            // MAIN INFORMATIONS
-            //====================================================================//
-            case 'ref':
-                if ($this->AttributeId) {
-                    $this->setSimple("reference", $fieldData, "Attribute");
-                } else {
-                    $this->setSimple("reference", $fieldData);
-                }
-
-                break;
-            default:
-                return;
-        }
-        unset($this->in[$fieldName]);
     }
 }
