7c4885f67d36e8c98402808d4e75531bea4b5112
BadPixxel <eshop.bpaquier@gmail.com>
2020-11-09 19:23:52 +0100

    FIX: Composer 2

---
+++ travis/after.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+################################################################################
+#
+#  This file is part of SplashSync Project.
+#
+#  Copyright (C) Splash Sync <www.splashsync.com>
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  For the full copyright and license information, please view the LICENSE
+#  file that was distributed with this source code.
+#
+#  @author Bernard Paquier <contact@splashsync.com>
+#
+################################################################################
+
+echo "----------------------------------------------------"
+echo "--> After Script"
+echo "----------------------------------------------------"
+
+echo "Composer ==> Outdated Packages"
+composer outdated -D
+
+echo "PhpLoc ==> Packages Statistics"
+wget https://phar.phpunit.de/phploc.phar -q
+php phploc.phar modules/splashsync --exclude=modules/splashsync/vendor
+rm phploc.phar
