41815c46d7a0b0005711205497dbf220e7a52d52
BadPixxel <eshop.bpaquier@gmail.com>
2022-06-01 16:55:18 +0200

    Release of V1.7.2

--- modules/splashsync/splashsync.php
+++ modules/splashsync/splashsync.php
@@ -74,7 +74,7 @@ class SplashSync extends Module
         // Init Module Main Information Fields
         $this->name = 'splashsync';
         $this->tab = 'administration';
-        $this->version = '1.7.1';
+        $this->version = '1.7.2';
         $this->author = 'SplashSync';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = array('min' => '1.6', 'max' => '1.7');
