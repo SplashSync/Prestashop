e9cd06acdb36bc68a7ebcb91f9d590253503b2b3
BadPixxel <eshop.bpaquier@gmail.com>
2023-11-27 23:04:05 +0100

    Reverse Orders & Invoice Listing Sort Order

--- modules/splashsync/src/Services/Images/ThumbnailUpdater.php
+++ modules/splashsync/src/Services/Images/ThumbnailUpdater.php
@@ -38,6 +38,7 @@ class ThumbnailUpdater
         if (!file_exists($imagePath)) {
             return;
         }
+
         //====================================================================//
         // Fetch Defined Image Formats
         try {
