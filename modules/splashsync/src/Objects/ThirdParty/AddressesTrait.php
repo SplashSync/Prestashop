a73fbbb95ef4f33ebb04cf82086d9cfb596a2851
BadPixxel <eshop.bpaquier@gmail.com>
2025-03-17 18:23:38 +0100

    Improve user address collection by integrating multiple sources
    Refactored the address collection logic to gather addresses from both the address and order tables. Also added error handling for database queries and ensured robustness by filtering addresses. This change enhances the reliability and completeness of the address data.

--- modules/splashsync/src/Objects/ThirdParty/AddressesTrait.php
+++ modules/splashsync/src/Objects/ThirdParty/AddressesTrait.php
@@ -80,22 +80,96 @@ trait AddressesTrait
             $this->out["contacts"] = array();
         }
         //====================================================================//
-        // Read Address List from Database (Also Collect Deleted Addresses)
-        $sql = 'SELECT DISTINCT a.* FROM `'._DB_PREFIX_.'address` a 
-                    WHERE `id_customer` = '.(int) $this->object->id
-        ;
-        $addressList = Db::getInstance()->executeS($sql);
+        // Collect All User Addresses
+        // - from Address Table (Also Collect Deleted Addresses)
+        // - from Order Table (Addresses used on Orders)
+        $addressIds = array_unique(array_replace_recursive(
+            $this->getAddressIdsFromAddressTable(),
+            $this->getAddressIdsFromOrderTable()
+        ));
         //====================================================================//
         // If Address List Is Empty => Null
-        if (empty($addressList)) {
+        if (empty($addressIds)) {
             return;
         }
         //====================================================================//
         // Run Through Address List
-        foreach ($addressList as $index => $address) {
+        foreach ($addressIds as $index => $addressId) {
             $this->out["contacts"][$index] = array(
-                "address" => self::objects()->encode("Address", $address["id_address"])
+                "address" => self::objects()->Encode("Address", (string) $addressId)
             );
         }
+    }
+
+    /**
+     * Get List of User Addresses from Address Table
+     *
+     * @return int[]
+     */
+    private function getAddressIdsFromAddressTable(): array
+    {
+        $addressIds = array();
+        //====================================================================//
+        // Read Address List from Database (Also Collect Deleted Addresses)
+        $sql = 'SELECT DISTINCT a.id_address FROM `'._DB_PREFIX_.'address` a 
+                    WHERE `id_customer` = '.(int) $this->object->id;
+
+        try {
+            $addressList = Db::getInstance()->executeS($sql);
+        } catch (PrestaShopDatabaseException $e) {
+            return $addressIds;
+        }
+        //====================================================================//
+        // If Address List Is Empty => Null
+        if (empty($addressList) || !is_iterable($addressList)) {
+            return $addressIds;
+        }
+        //====================================================================//
+        // Run Through Address List
+        foreach ($addressList as $address) {
+            if ($addressId = (int) $address["id_address"] ?? null) {
+                $addressIds[$addressId] = $addressId;
+            }
+        }
+
+        return array_filter($addressIds);
+    }
+
+    /**
+     * Get List of User Addresses from Order Table
+     *
+     * @return int[]
+     */
+    private function getAddressIdsFromOrderTable(): array
+    {
+        $addressIds = array();
+        //====================================================================//
+        // Read Address List from Orders Table
+        $sql = 'SELECT DISTINCT o.id_address_delivery, o.id_address_invoice 
+                    FROM `'._DB_PREFIX_.'orders` o 
+                    WHERE `id_customer` = '.(int) $this->object->id;
+
+        try {
+            $addressList = Db::getInstance()->executeS($sql);
+        } catch (PrestaShopDatabaseException $e) {
+            return $addressIds;
+        }
+        //====================================================================//
+        // If Address List Is Empty => Null
+        if (empty($addressList) || !is_iterable($addressList)) {
+            return $addressIds;
+        }
+        //====================================================================//
+        // Run Through Address List
+        foreach ($addressList as $address) {
+            if ($addressId = (int) $address["id_address_delivery"] ?? null) {
+                $addressIds[$addressId] = $addressId;
+            }
+            if ($addressId = (int) $address["id_address_invoice"] ?? null) {
+                $addressIds[$addressId] = $addressId;
+            }
+        }
+
+        return array_filter($addressIds);
     }
 }
