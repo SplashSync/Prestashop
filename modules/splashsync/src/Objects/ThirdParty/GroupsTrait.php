8cba5202af5bdb42d131e61a190995a4d61316aa
BadPixxel <eshop.bpaquier@gmail.com>
2025-09-10 16:00:06 +0200

    Add support for managing customer groups and default group updates

--- modules/splashsync/src/Objects/ThirdParty/GroupsTrait.php
+++ modules/splashsync/src/Objects/ThirdParty/GroupsTrait.php
@@ -36,7 +36,7 @@ trait GroupsTrait
     protected function buildGroupsFields(): void
     {
         //====================================================================//
-        // Firstname
+        // Customer Groups
         $this->fieldsFactory()->create(SPL_T_INLINE)
             ->identifier('groups')
             ->name(SLM::translate('Groups', 'AdminGlobal'))
@@ -45,10 +45,19 @@ trait GroupsTrait
             ->addChoices(GroupsManager::getAllGroupNames())
             ->isReadOnly()
         ;
+        //====================================================================//
+        // Customer Group
+        $this->fieldsFactory()->create(SPL_T_VARCHAR)
+            ->identifier('group')
+            ->name(SLM::translate('Group', 'AdminGlobal'))
+            ->group('Meta')
+            ->microData('http://schema.org/Organization', 'category')
+            ->addChoices(GroupsManager::getAllGroupNames())
+        ;
     }
 
     /**
-     * Read requested Field
+     * Read the requested fields
      *
      * @param string $key       Input List Key
      * @param string $fieldName Field Identifier / Name
@@ -64,13 +73,58 @@ trait GroupsTrait
                 $this->out[$fieldName] = InlineHelper::fromArray(
                     GroupsManager::getGroupNames($this->object)
                 );
-
                 GroupsManager::getAllGroupNames();
+
+                break;
+            case 'group':
+                $group = GroupsManager::getGroup($this->object->id_default_group);
+                $this->out[$fieldName] = null;
+                if ($group && is_string($group->name)) {
+                    $this->out[$fieldName] = strtolower($group->name);
+                }
 
                 break;
             default:
                 return;
         }
         unset($this->in[$key]);
+    }
+
+    /**
+     * Write Given Fields
+     *
+     * @param string      $fieldName Field Identifier / Name
+     * @param null|string $fieldData Field Data
+     *
+     * @return void
+     */
+    protected function setGroupsFields(string $fieldName, ?string $fieldData): void
+    {
+        //====================================================================//
+        // WRITE Fields
+        switch ($fieldName) {
+            case 'group':
+                unset($this->in[$fieldName]);
+                $newGroup = GroupsManager::getGroupByName($fieldData);
+                //====================================================================//
+                // Nothing to Update
+                if (!$newGroup || ($newGroup->id == $this->object->id_default_group)) {
+                    break;
+                }
+                //====================================================================//
+                // Update Groups List
+                /** @var int[] $groups */
+                $groups = $this->object->getGroups();
+                $this->object->updateGroup(array_unique(array_merge(
+                    $groups,
+                    array($newGroup->id)
+                )));
+                //====================================================================//
+                // Update Default Group
+                $this->object->id_default_group = (int) $newGroup->id;
+                $this->needUpdate();
+
+                break;
+        }
     }
 }
