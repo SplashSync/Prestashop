b7b1a1131c7fe42b0063baccd151b34eac4c5d46
BadPixxel <eshop.bpaquier@gmail.com>
2026-06-26 15:21:25 +0200

    Release v2.9.3: update module version and configuration files.

--- modules/splashsync/splashsync.php
+++ modules/splashsync/splashsync.php
@@ -76,7 +76,7 @@ class SplashSync extends Module
         // Init Module Main Information Fields
         $this->name = 'splashsync';
         $this->tab = 'administration';
-        $this->version = '2.9.2';
+        $this->version = '2.9.3';
         $this->author = 'SplashSync';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = array('min' => '1.7', 'max' => '9.99.99');
